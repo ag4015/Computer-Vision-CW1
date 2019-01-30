7c2220a8fefd5e8a582098e32b8753539daafc55
gilson15 <ag4015@ic.ac.uk>
2019-01-30 18:37:59 +0000

    Load csv descriptor into python variable

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
 import sys
+from sklearn.cluster import KMeans
+import numpy as np
+import csv
 
 desc_filename = str(sys.argv[1])
 
 
 # Get data descriptors from Caltech using MATLAB
 os.system("cd RF_2019; matlab -nodesktop -nosplash -r \"run(\'init.m\');clc;get_descriptors(\'CalTech\', \'" + desc_filename + "\', 0);exit;\"")
-# os.system('cd ..')
+
+# Load descriptors from csv into python variable
+with open(str("RF_2019/" + desc_filename), 'rt') as csvfile:
+    descriptors = np.zeros((100000,128)) 
+    csv.field_size_limit(sys.maxsize)
+    f = csv.reader(csvfile, delimiter=',')
+    i = 0
+    for row in f:
+        descriptors[:,i] = row  
+        i = i + 1
+
