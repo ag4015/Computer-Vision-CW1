0790ad298b5951700a6a314aa289e1d02b6178c4
gilson15 <ag4015@ic.ac.uk>
2019-01-31 13:47:38 +0000

    Compute codeword vocabulary

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from sklearn.cluster import KMeans
 import numpy as np
 import csv
+import pickle
 
 desc_filename = str(sys.argv[1])
 get_descriptors = int(sys.argv[2])
@@ -20,6 +21,23 @@ with open(str("RF_2019/" + desc_filename), 'rt') as csvfile:
     for row in f:
         descriptors[:,i] = row  
         i = i + 1
-
+#
 num_clusters = 256
-kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_jobs = 3).fit(descriptors)
+
+# kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_jobs = 3).fit(descriptors)
+# pickle_out = open('kmeans.pickle', 'wb')
+# pickle.dump(kmeans,pickle_out) 
+# pickle_out.close()
+#
+pickle_in = open('kmeans.pickle', 'rb')
+kmeans = pickle.load(pickle_in)
+
+# Get all the descriptors in the same cluster and average them
+codewords = np.zeros((num_clusters,128))
+for n in range(0, max(kmeans.labels_)):
+    cluster = np.where(kmeans.labels_==n)
+    for i in range(0,128):
+        codewords[n,i] = np.mean(descriptors[cluster[0],i])
+
+
+
