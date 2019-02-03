02c823c5a00c69d65a3618540a03d7379777f483
gilson15 <ag4015@ic.ac.uk>
2019-02-03 15:59:28 +0000

    begin q2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import cv2
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn import cluster
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import csv
 import pickle
@@ -17,6 +18,8 @@ def bag_of_words_histogram(desc, kmeans):
         for image_descriptors in class_row:
             descriptors_nearest_clusters = kmeans.predict(image_descriptors.T)
             histogram = np.bincount(descriptors_nearest_clusters)
+            if len(histogram) < 256:
+                print(len(histogram))
             class_histogram.append(histogram)
         bags_of_words.append(class_histogram)
     return np.array(bags_of_words) #converts list of lists into a numpy array
@@ -39,7 +42,7 @@ else:  #if no arguments were given (too lazy), use the following ones
 if compute_kmeans:
     print('Computing K-Means...')
     num_clusters = 256
-    kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_jobs = 3).fit(desc_sel)
+    kmeans = cluster.KMeans(n_clusters=num_clusters, random_state=0, n_jobs = 3).fit(desc_sel)
     pickle_out = open('kmeans.pickle', 'wb')
     pickle.dump(kmeans,pickle_out) 
     pickle_out.close()
@@ -60,7 +63,32 @@ data_test = bag_of_words_histogram(desc_te, kmeans)
 #all code up to here is what would be in the getData.m file 
 
 #an example of a training set image histogram would be
-plt.plot(data_train[8][6]) #training image class 9 image 7
-plt.show()
+# plt.plot(data_train[8][6]) #training image class 9 image 7
+# plt.show()
 
 print("done")
+
+# Construct the lables from the training data 1 lable == 1 class
+train_lables = np.zeros(15)
+training_data = np.zeros((10*15,256))
+for n in range(1,10):
+    train_lables = np.r_[train_lables, n*np.ones(15)]
+
+# Format train_data
+for i in range(0,10):
+    try:
+        for n in range(0,15):
+            training_data[i*15+n] = data_train[i][n]
+    except Exception:
+        print(i,n)
+
+
+import pdb; pdb.set_trace()
+
+clf = RandomForestClassifier(n_estimators=10, max_depth=5,random_state=0)
+clf.fit(data_train, train_lables)
+
+
+
+
+
