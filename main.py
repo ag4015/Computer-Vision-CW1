8105c03fe25eee31a11159e784f22044719320c8
gilson15 <ag4015@ic.ac.uk>
2019-02-04 12:37:14 +0000

    Test vocabulary sizes

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import pickle
 import scipy.io
 import matplotlib.pyplot as plt
 
-def bag_of_words_histogram(desc, kmeans):
+def bag_of_words_histogram(desc, kmeans, num_clusters):
     #this function creates all bags of words for the entire input set (either training or test set)
     bags_of_words = []
     for class_row in desc:
         class_histogram = []
         for image_descriptors in class_row:
             descriptors_nearest_clusters = kmeans.predict(image_descriptors.T)
-            histogram = np.bincount(descriptors_nearest_clusters, minlength=256)
+            histogram = np.bincount(descriptors_nearest_clusters, minlength=num_clusters)
             class_histogram.append(histogram)
         bags_of_words.append(class_histogram)
     return np.array(bags_of_words) #converts list of lists into a numpy array
@@ -28,6 +28,31 @@ def print_all_CV_scores(clf):
             ' mean_fit_time:', mean_fit_time,  #given in seconds
             ' mean_score_time:', mean_score_time)  #given in seconds
 
+def test_vocabulary(desc_sel, desc_tr, desc_te):
+    vocabulary_sizes = [50,100,200,300,400]
+    score_list = []
+    for num_clusters in vocabulary_sizes:
+        print('Computing codebook for a vocabulary of', num_clusters) 
+        kmeans = cluster.KMeans(n_clusters=num_clusters, random_state=0, n_jobs = 3).fit(desc_sel)
+        
+        # Construct training data lables
+        train_lables = [i//15 for i in range(150)]
+        test_lables = train_lables #in this case, since both are 10x15 images
+
+        # Calculate the bag of words for training and test data
+        data_train = bag_of_words_histogram(desc_tr, kmeans, num_clusters).reshape(150,num_clusters)
+        data_test = bag_of_words_histogram(desc_te, kmeans, num_clusters).reshape(150,num_clusters)
+
+        print('Computing RF for a vocabulary of', num_clusters) 
+        # Use best performing parameters for RF
+        RFC = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0).fit(data_train, train_lables)
+        score = RFC.score(data_test, test_lables)
+        score_list.append(score)
+        print('score:', score)
+    pickle_out = open('vocabulary_scores.pickle', 'wb')
+    pickle.dump(score_list, pickle_out)
+    pickle_out.close()
+
 #load variables from matlab data
 desc_tr = scipy.io.loadmat('matlab_data/desc_tr.mat')['desc_tr']
 desc_sel = scipy.io.loadmat('matlab_data/desc_sel.mat')['desc_sel'].T
@@ -36,15 +61,18 @@ imgIdx = scipy.io.loadmat('matlab_data/imgIdx.mat')['imgIdx'].T
 imgIdx_tr = scipy.io.loadmat('matlab_data/imgIdx_tr.mat')['imgIdx_tr'].T
 imgIdx_te = scipy.io.loadmat('matlab_data/imgIdx_te.mat')['imgIdx_te'].T
 
-if len(sys.argv) > 1:  
+if len(sys.argv) ==  2:  
     compute_kmeans = int(sys.argv[1])
+if len(sys.argv) ==  3:
+    compute_search = int(sys.argv[2])
 else:  #if no arguments were given (too lazy), use the following ones
     compute_kmeans = 0
+    compute_search = 0
 
 
+num_clusters = 256
 if compute_kmeans:
     print('Computing K-Means...')
-    num_clusters = 256
     kmeans = cluster.KMeans(n_clusters=num_clusters, random_state=0, n_jobs = -1).fit(desc_sel)
     pickle_out = open('kmeans.pickle', 'wb')
     pickle.dump(kmeans,pickle_out) 
@@ -57,8 +85,8 @@ codewords = kmeans.cluster_centers_.copy()
 
 #classes = ['tick', 'trilobite', 'umbrella', 'watch', 'water_lilly', 'wheelchair', 'wild_cat', 'windsor_chair', 'wrench', 'yin_yang']
 
-data_train = bag_of_words_histogram(desc_tr, kmeans)
-data_test = bag_of_words_histogram(desc_te, kmeans)
+data_train = bag_of_words_histogram(desc_tr, kmeans, num_clusters)
+data_test = bag_of_words_histogram(desc_te, kmeans, num_clusters)
 
 #data_train contains the bags of words of the entire training set
 #data_test contains the bags of words of the entire test set
@@ -71,26 +99,46 @@ data_test = bag_of_words_histogram(desc_te, kmeans)
 
 print("done")
 
-# Construct training data labels
-train_labels = [i//15 for i in range(150)]
-test_labels = train_labels #in this case, since both are 10x15 images
+# Construct training data lables
+train_lables = [i//15 for i in range(150)]
+test_lables = train_lables #in this case, since both are 10x15 images
 
 # Flatten out train and test data (preparing them for RF classifier)
-training_data = data_train.reshape(150, 256)
-test_data = data_test.reshape(150, 256)
+training_data = data_train.reshape(150, num_clusters)
+test_data = data_test.reshape(150, num_clusters)
 
 
 RFC = RandomForestClassifier(n_estimators=100, max_depth=5,random_state=0)
 parameters = {'n_estimators':[10, 100], 'max_depth':[5, 10, 20]}
-clf = GridSearchCV(RFC, param_grid=parameters, cv=3, return_train_score=False)
-clf.fit(training_data, train_labels)
+
+# Compute GridSearchCV or load it from file
+if compute_search:
+    clf = GridSearchCV(RFC, param_grid=parameters, cv=3, return_train_score=False,n_jobs=3)
+    clf.fit(training_data, train_lables)
+    pickle_out = open('clf.pickle', 'wb')
+    pickle.dump(clf, pickle_out)
+    pickle_out.close()
+else:
+    pickle_in = open('clf.pickle', 'rb')
+    clf = pickle.load(pickle_in)
+    pickle_in.close()
+
 predictions = clf.predict(test_data)
 reshaped_preds = predictions.reshape(10, 15)
 print(reshaped_preds)
 
-print('score:', clf.score(test_data, test_labels))
+print('score:', clf.score(test_data, test_lables))
 
 #print scores and times for all parameters tested
 print_all_CV_scores(clf)
 
 print("done")
+
+test_vocabulary(desc_sel, desc_tr, desc_te)
+
+
+
+
+
+
+
